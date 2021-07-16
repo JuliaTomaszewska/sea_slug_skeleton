ccd0ae546405479d1f5cc6fa97706cf309f03817
Julia Tomaszewska <jdt@scc1.bu.edu>
2021-07-16 07:48:19 -0400

    fixed y calculation

--- skeleton_habituationGWR.py
+++ skeleton_habituationGWR.py
@@ -41,12 +41,11 @@ y = np.zeros((1,nTs)) # set up (define) a vector for the output time series
 #     then indent 4 spaces and write the equation that
 #     describes how each input value in the vector x is 
 #     transformed to the output value in the vector y
-t = 0
+
 for i in range(0, nTs):
-    if  x[t]>0:
+    if  x[i]>0:
          v = v*0.7
-    y[0,t]=v*x[t]
-    t +=1
+    y[0,i]=v*x[i]
 
 
 
